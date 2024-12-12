ab3085ba958c2dfb11127a81242924a003dcdd9e
Surya <surya.nata.aardhana@gmail.com>
2024-12-12 15:04:12 +0700

    style: fetch products api

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,17 +1,38 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
+type itemType = {
+    id: number;
+    name: string;
+    price: number;
+    size: string;
+};
+
 const ProductsPage = () => {
     const { push, query } = useRouter();
     const [isLogin, setIsLogin] = useState(true);
+    const [data, setData] = useState([]);
     useEffect(() => {
         if (!isLogin) {
             push("/auth/login");
         }
     }, []);
+
+    useEffect(() => {
+        fetch("/api/products")
+            .then((res) => res.json())
+            .then((respons) => {
+                setData(respons.data);
+            });
+    }, []);
     return (
         <div>
             <h1>Ini list products</h1>
+            <ul>
+                {data.map((item: itemType) => (
+                    <li key={item.id}>{item.name}</li>
+                ))}
+            </ul>
         </div>
     );
 };
