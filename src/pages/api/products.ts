34ab36f6c4ed9eb94f82d80f635ed4c0bb85b811
Surya <surya.nata.aardhana@gmail.com>
2024-12-19 08:08:28 +0700

    feat: api products

--- src/pages/api/products.ts
+++ src/pages/api/products.ts
@@ -1,34 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { getData } from "@/lib/firebase/service";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
     status: boolean;
     statusCode: number;
-    data: {
-        id: number;
-        name: string;
-        price: number;
-        size: string;
-    }[];
+    data: any;
 };
 
-export default function handler(
+export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>
 ) {
-    const data = [
-        {
-            id: 1,
-            name: "mentega",
-            price: 18,
-            size: "XL",
-        },
-        {
-            id: 2,
-            name: "kopi",
-            price: 18,
-            size: "XL",
-        },
-    ];
+    const data = await getData("products");
     res.status(200).json({ status: true, statusCode: 200, data });
 }
