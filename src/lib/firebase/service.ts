9d7cfa497305ed7d6c668cea339d9c46e65178e3
Surya <surya.nata.aardhana@gmail.com>
2024-12-19 08:08:10 +0700

    feat: get firestore data func

---
+++ src/lib/firebase/service.ts
@@ -0,0 +1,13 @@
+import { collection, getDocs, getFirestore } from "firebase/firestore";
+import app from "./init";
+
+const firestore = getFirestore(app);
+
+export async function getData(collectionName: string) {
+    const snapshot = await getDocs(collection(firestore, collectionName));
+    const data = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+    }));
+    return data;
+}
